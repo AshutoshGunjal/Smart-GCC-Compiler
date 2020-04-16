b5800473c88d256756ad81d959dc35cf5cb1fa4f
AshutoshGunjal <ashutosh.r.gunjal@gmail.com>
2020-04-16 18:40:00 -0400

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Complex.h"
+
+
+using namespace std;
+ 
+ 
+ int main()
+ {
+
+cout<<"Creating Complex class"<<endl;
+
+Complex c1(3), c2(3,4);
+
+cout<<"c2: "<<c2.getReal()<<"<"<<c2.getImg()<<">"<<endl;
+     
+ 
+ return 0;
+ }
